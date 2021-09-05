f0d6e51a4b99230bf1eb5191cf4e0a7ddbed2c7c
Ganesh Reddy <reddyganesh125@gmail.com>
2021-09-06 01:14:48 +0530

    Added:Multiple Persons

--- src/main/java/com/Blz/assignment/AddressBookMain.java
+++ src/main/java/com/Blz/assignment/AddressBookMain.java
@@ -4,95 +4,109 @@ import java.util.Scanner;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        Contact n = new Contact();
+        String firstName, lastName, address, state, city,pinCode, phoneNumber, email;
+        Contact[] n = new Contact[4];
         Scanner input = new Scanner(System.in);
+        int i;
+        for (i=1;i<4;i++) {
+            n[i] = new Contact();
 
-        System.out.println("Enter the FirstName");
-        String firstName = input.next();
-        n.setFirstName(firstName);
+            System.out.println("Enter details of Contact "+i+"\n");
 
-        System.out.println("Enter the LastName");
-        String lastName = input.next();
-        n.setLastName(lastName);
+            System.out.println("Enter the FirstName");
+            firstName = input.next();
+            n[i].setFirstName(firstName);
 
-        System.out.println("Enter the Address");
-        String address = input.next();
-        n.setAddress(address);
+            System.out.println("Enter the LastName");
+            lastName = input.next();
+            n[i].setLastName(lastName);
 
-        System.out.println("Enter the city");
-        String state = input.next();
-        n.setState(state);
+            System.out.println("Enter the Address");
+            address = input.next();
+            n[i].setAddress(address);
 
-        System.out.println("Enter the city");
-        String city = input.next();
-        n.setCity(city);
+            System.out.println("Enter the state");
+            state = input.next();
+            n[i].setState(state);
 
-        System.out.println("Enter the Pincode");
-        String pinCode = input.next();
-        n.setZip(pinCode);
+            System.out.println("Enter the city");
+            city = input.next();
+            n[i].setCity(city);
 
-        System.out.println("Enter the Phone Number");
-        String phoneNumber = input.next();
-        n.setPhoneNumber(phoneNumber);
+            System.out.println("Enter the Pincode");
+            pinCode = input.next();
+            n[i].setZip(pinCode);
 
-        System.out.println("Enter the Email");
-        String email = input.next();
-        n.setEmail(email);
+            System.out.println("Enter the Phone Number");
+            phoneNumber = input.next();
+            n[i].setPhoneNumber(phoneNumber);
 
-        System.out.println(n.getFirstName());
-        System.out.println(n.getLastName());
-        System.out.println(n.getAddress());
-        System.out.println(n.getState());
-        System.out.println(n.getCity());
-        System.out.println(n.getZip());
-        System.out.println(n.getPhoneNumber());
-        System.out.println(n.getEmail());
+            System.out.println("Enter the Email");
+            email = input.next();
+            n[i].setEmail(email);
 
-        System.out.println("Enter 1 to  edit\nEnter 2 to delete");
+
+
+            System.out.println("\nYour first name is " + n[i].getFirstName());
+            System.out.println("Your last name is " + n[i].getLastName());
+            System.out.println("Your address  is " + n[i].getAddress());
+            System.out.println("Your state is " + n[i].getState());
+            System.out.println("Your city is " + n[i].getCity());
+            System.out.println("Your zipcode is " + n[i].getZip());
+            System.out.println("Your phone number is " + n[i].getPhoneNumber());
+            System.out.println("Your email is " + n[i].getEmail());
+            System.out.println("\n");
+        }
+
+
+        System.out.println("Enter 1 to  edit\nEnter 2 to delete\nEnter 3 to Print All details");
+
         int choice = input.nextInt();
         if (choice == 1) {
+            System.out.println("Enter the Batch number");
+            int temp = input.nextInt();
             System.out.println("Enter\n 1 to edit FirstName \n 2 to edit LastName\n 3 to edit address\n 4 to edit State \n 5 to edit city \n 6 to edit Zipcode \n 7 to edit Phone Number \n 8 to edit email \n 9 to edit nothing");
             int edit = input.nextInt();
             switch (edit) {
                 case 1:
                     System.out.println("Enter New First Name");
-                    firstName = input.next();
-                    n.setFirstName(firstName);
+                    firstName= input.next();
+                    n[temp].setFirstName(firstName);
                     break;
                 case 2:
                     System.out.println("Enter new Last Name");
                     lastName = input.next();
-                    n.setLastName(lastName);
+                    n[temp].setLastName(lastName);
                     break;
                 case 3:
                     System.out.println("Enter new Address");
                     address = input.next();
-                    n.setAddress(address);
+                    n[temp].setAddress(address);
                     break;
                 case 4:
                     System.out.println("Enter the  new State");
                     state = input.next();
-                    n.setState(state);
+                    n[temp].setState(state);
                     break;
                 case 5:
                     System.out.println("Enter the new city");
                     city = input.next();
-                    n.setCity(city);
+                    n[temp].setCity(city);
                     break;
                 case 6:
                     System.out.println("Enter the new pincode");
                     pinCode = input.next();
-                    n.setZip(pinCode);
+                    n[temp].setZip(pinCode);
                     break;
                 case 7:
                     System.out.println("Enter new phone number");
                     phoneNumber = input.next();
-                    n.setPhoneNumber(phoneNumber);
+                    n[temp].setPhoneNumber(phoneNumber);
                     break;
                 case 8:
                     System.out.println("Enter new email");
                     email = input.next();
-                    n.setEmail(email);
+                    n[temp].setEmail(email);
                     break;
 
                 case 9:
@@ -104,23 +118,52 @@ public class AddressBookMain {
                     break;
 
             }
-            System.out.println("Your first name is "+n.getFirstName());
-            System.out.println("Your last name is "+n.getLastName());
-            System.out.println("Your address  is "+n.getAddress());
-            System.out.println("Your state is "+n.getState());
-            System.out.println("Your city is "+n.getCity());
-            System.out.println("Your zipcode is "+n.getZip());
-            System.out.println("Your phone number is "+n.getPhoneNumber());
-            System.out.println("Your email is "+n.getEmail());
-        }
-        else if (choice == 2)
-        {
-            System.out.println("Are you Sure to delete the contact? enter 1 to delete");
-            int delete = input.nextInt();
-            if (delete == 1) {
-                n = null;
-                System.out.println("Object is succesfully deleted!!!");
+            for (int j=1;j<4;j++) {
+                System.out.println("details of Contact "+j+"\n");
+                System.out.println("Your first name is " + n[j].getFirstName());
+                System.out.println("Your last name is " + n[j].getLastName());
+                System.out.println("Your address  is " + n[j].getAddress());
+                System.out.println("Your state is " + n[j].getState());
+                System.out.println("Your city is " + n[j].getCity());
+                System.out.println("Your zipcode is " + n[j].getZip());
+                System.out.println("Your phone number is " + n[j].getPhoneNumber());
+                System.out.println("Your email is " + n[j].getEmail() + "\n");
             }
+
+        } else if (choice == 2) {
+            System.out.println("Enter the batch you want to delete ");
+            int temp = input.nextInt();
+           n[temp]= null;
+                System.out.println("\nContact is succesfully deleted!!!" + "\n");
+            for (int j=1;j<4;j++) {
+                if(j!=temp) {
+                    System.out.println("Details of contact " + j+"\n");
+                    System.out.println("Your first name is " + n[j].getFirstName());
+                    System.out.println("Your last name is " + n[j].getLastName());
+                    System.out.println("Your address  is " + n[j].getAddress());
+                    System.out.println("Your state is " + n[j].getState());
+                    System.out.println("Your city is " + n[j].getCity());
+                    System.out.println("Your zipcode is " + n[j].getZip());
+                    System.out.println("Your phone number is " + n[j].getPhoneNumber());
+                    System.out.println("Your email is " + n[j].getEmail() + "\n");
+                }
+            }
+
+            }
+        else if (choice == 3){
+            for (int j=1;j<4;j++) {
+                System.out.println("Details of contact "+j+"\n");
+                System.out.println("Your first name is " + n[j].getFirstName());
+                System.out.println("Your last name is " + n[j].getLastName());
+                System.out.println("Your address  is " + n[j].getAddress());
+                System.out.println("Your state is " + n[j].getState());
+                System.out.println("Your city is " + n[j].getCity());
+                System.out.println("Your zipcode is " + n[j].getZip());
+                System.out.println("Your phone number is " + n[j].getPhoneNumber());
+                System.out.println("Your email is " + n[j].getEmail() + "\n");
+            }
+
+        }
 
 
         }
@@ -128,4 +171,3 @@ public class AddressBookMain {
 
     }
 
-}
